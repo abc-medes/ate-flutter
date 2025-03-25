e506c9f7c8586a3598d854f269e0a414bc814c09
gwangbaekun <gwangbaekun@gmail.com>
2025-03-25 22:11:08 +0900

    FEAT] Test button to login with auth notifier / adopt later

--- lib/features/auth/views/login_view.dart
+++ lib/features/auth/views/login_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:ate_project/core/services/auth_service.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends ConsumerWidget {
   const LoginView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authState = ref.watch(authStateProvider);
+    final authNotifier = ref.read(authStateProvider.notifier);
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(24.0),
@@ -24,7 +30,13 @@ class LoginView extends StatelessWidget {
             ),
             const SizedBox(height: 24),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () async {
+                await authNotifier.login(
+                    userId: "123", email: "test@example.com");
+                if (authState.isAuthenticated) {
+                  context.go('/intro'); // Navigate after login
+                }
+              },
               child: const Text('Login'),
             ),
           ],

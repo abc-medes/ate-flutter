148bb86bbd2f4f47ff2619c811b51eabe9d55cc4
gwangbaekun <gwangbaekun@gmail.com>
2025-04-23 22:44:53 +0900

    [REFACTOR] home view with suggestions and daily inputs. divided it but where to display not decided

--- lib/data/repositories/health_repository.dart
+++ lib/data/repositories/health_repository.dart
@@ -24,14 +24,87 @@ class HealthRepository {
 
       final userInputData = healthData['user_input_data'];
 
-      final tempData = UserInputData();
-      tempData.setField(field, true);
-
-      final jsonMap = tempData.toJson();
-
-      return jsonMap.keys.any((key) => userInputData.containsKey(key));
+      String fieldName = _getFieldJsonKey(field);
+      return userInputData.containsKey(fieldName);
     } catch (e) {
       return false;
     }
+  }
+
+  String _getFieldJsonKey(UserInputField field) {
+    switch (field) {
+      case UserInputField.height:
+        return 'height';
+      case UserInputField.weight:
+        return 'weight';
+      case UserInputField.dateOfBirth:
+        return 'date_of_birth';
+      case UserInputField.gender:
+        return 'gender';
+      case UserInputField.preExistingConditions:
+        return 'pre_existing_conditions';
+      case UserInputField.medications:
+        return 'medications';
+      case UserInputField.allergies:
+        return 'allergies';
+      case UserInputField.nutritionData:
+        return 'nutrition_data';
+      case UserInputField.moodData:
+        return 'mood_data';
+      case UserInputField.symptoms:
+        return 'symptoms';
+      case UserInputField.sleepQuality:
+        return 'sleep_quality';
+      case UserInputField.activityData:
+        return 'activity_data';
+    }
+  }
+
+  Future<bool> isBasicUserDataSaved(BasicUserData field) async {
+    return isUserInputFieldSaved(field.toUserInputField());
+  }
+
+  Future<List<UserInputField>> getMissingUserInputFields() async {
+    final missingFields = <UserInputField>[];
+
+    final importantFields = [
+      UserInputField.height,
+      UserInputField.weight,
+      UserInputField.dateOfBirth,
+      UserInputField.gender,
+      UserInputField.preExistingConditions,
+      UserInputField.medications,
+      UserInputField.allergies,
+    ];
+
+    for (final field in importantFields) {
+      final isSaved = await isUserInputFieldSaved(field);
+      if (!isSaved) {
+        missingFields.add(field);
+      }
+    }
+
+    print(missingFields);
+
+    return missingFields;
+  }
+
+  Future<List<BasicUserData>> getMissingBasicUserData() async {
+    final missingFields = <BasicUserData>[];
+
+    for (final field in BasicUserData.values) {
+      final isSaved = await isBasicUserDataSaved(field);
+      if (!isSaved) {
+        missingFields.add(field);
+      }
+    }
+
+    print(missingFields);
+
+    return missingFields;
+  }
+
+  List<DailyUserData> getDailyUserDataFields() {
+    return DailyUserData.values;
   }
 }
